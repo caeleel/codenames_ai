b459dd7587400483b52701b0e69b15b72fe92508
Karl Jiang <karl@justin.tv>
2016-09-02 22:12:09 -0700

    finished

--- engine.py
+++ engine.py
@@ -1,4 +1,5 @@
 import json
+import random
 import sys
 import itertools
 
@@ -6,16 +7,46 @@ def load_words(filename):
 	with open(filename, 'r') as f:
 		return json.loads(f.read())
 
+def possible_words(combos, meaning_sets):
+	print 'creating word pool'
+	word_pool = set()
+
+	for test_set in combos:
+		available = meaning_sets[test_set[0]]
+		for x in test_set[1:]:
+			available = available.intersection(meaning_sets[x])
+		word_pool = word_pool.union(available)
+	
+	return word_pool
+
+def shuffle(words, n):
+	for i in xrange(n):
+		j = random.randint(i, len(words)-1)
+		tmp = words[i]
+		words[i] = words[j]
+		words[j] = tmp
+	return words[:n]
+
 if __name__ == '__main__':
 	print 'loading board file'
-	board_file = sys.argv[1]
-	with open(board_file, 'r') as f:
-		lines = f.read().split('\n')
-		board = lines[0].split(' ')
-		red = lines[1].split(' ')
-		blue = lines[2].split(' ')
-		black = lines[3].split(' ')
-		white = [x for x in board if x not in red + blue + black]
+	if len(sys.argv) > 1:
+		board_file = sys.argv[1]
+		with open(board_file, 'r') as f:
+			lines = f.read().split('\n')
+			board = lines[0].split(' ')
+			red = lines[1].split(' ')
+			blue = lines[2].split(' ')
+			black = lines[3].split(' ')
+			other = [x for x in board if x not in red]
+	else:
+		with open('codewords.txt', 'r') as f:
+			codewords = f.read().lower().split('\n')
+			codewords = [x for x in codewords if x]
+			board = shuffle(codewords, 25)
+			red = shuffle(board, 9)
+			blue = board[9:17]
+			black = [board[17]]
+			other = board[9:]
 
 	print 'board :: {}'.format(board)
 	print 'red :: {}'.format(red)
@@ -26,11 +57,8 @@ if __name__ == '__main__':
 	assoc = load_words('associations.txt')
 	print 'creating meaning sets...'
 	meaning_sets = {}
-	meaning_index = {}
 	for k, v in assoc.iteritems():
 		meaning_sets[k] = set(v)
-		#for i, s in enumerate(v):
-		#	meaning_index[k + '-' + s] = i
 
 	print 'validating board'
 	for word in board:
@@ -42,53 +70,34 @@ if __name__ == '__main__':
 			print '{} is not on the board!'.format(word)
 			sys.exit(1)
 
-	combos = itertools.combinations(red, 4)
-	word_pool = set()
-
-	print 'creating word pool'
-	for test_set in combos:
-		available = meaning_sets[test_set[0]]
-		for x in test_set[1:]:
-			available = available.intersection(meaning_sets[x])
-		word_pool = word_pool.union(available)
+	combos = itertools.combinations(red, 6)
+	word_pool = possible_words(combos, meaning_sets)
 	
 	print 'counting candidates'
 	counts = []
 	for x in word_pool:
 		words = []
-		red_count = 0
-		blue_count = 0
-		black_count = 0
-		white_count = 0
-		index = -1
+		other_words = []
+		word_count = 0
+		other_count = 0
 		for b in red:
 			if x in meaning_sets[b]:
-				red_count += 1
-				#index *= meaning_index[b + '-' + x]
+				word_count += 1
 				words.append(b)
-		for b in blue:
+		for b in other:
 			if x in meaning_sets[b]:
-				blue_count += 1
-				words.append(b)
-		for b in black:
-			if x in meaning_sets[b]:
-				black_count += 1
-				words.append(b)
-		for b in white:
-			if x in meaning_sets[b]:
-				white_count += 1
-				words.append(b)
+				other_count += 1
+				other_words.append(b)
 	
-		counts.append([red_count, index, -black_count, -blue_count, -white_count, x, words])
+		counts.append([word_count, -other_count, x, words, other_words])
 	
 	counts.sort()
 	counts.reverse()
-	for count in counts[:200]:
-		print 'Distribution: ({} => {}) R:{} / b!:{} / B:{} / W:{}'.format(count[1], count[5], count[0], count[2], count[3], count[4])
-	#	for test_set in itertools.combinations(count[6], count[0]):
-	#		index = -1
-	#		for word in test_set:
-	#			index *= meaning_index[word + '-' + count[5]]
-	#		print 'COMPARING {} -> {}'.format(index, count[1])
-	#		if index > count[1]:
-	#			break
+	for count in counts[:10]:
+		print 'Distribution: ({}) W:{} / OW:{}'.format(count[2], count[0], count[1])
+		other_words = count[4]
+		other_word_pool = possible_words([other_words], meaning_sets)
+		negative = other_word_pool.difference(word_pool)
+		if negative:
+			print 'Negative word: {}'.format(negative.pop())
+			sys.exit(0)
